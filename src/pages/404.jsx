8835e5a588760c19fd6c89f2575ffe21dca08f1d
Romain Campech <romain.campech@gmail.com>
2020-03-16 11:37:26 +0000

    Add SEO component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import SEO from "../components/SEO"
 import Layout from "../components/Layout"
 
-const Home = () => (
+const NotFound = () => (
   <Layout>
-    <SEO />
-    <h1>Romain Campech</h1>
+    <SEO title="404" />
+    <h1>Not Found</h1>
   </Layout>
 )
 
-export default Home
+export default NotFound
